8843bf782467c63cd41707c3b86bc3b4a853ef58
Sage Gerard <sage@sagegerard.com>
2019-08-25 11:20:04 -0400

    Add missing dependencies

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "polyglot")
-(define deps '("base" "file-watchers" "txexpr" "unlike-assets"))
+(define deps '("base" "file-watchers" "txexpr" "unlike-assets" "aws" "markdown"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "An unlike compiler that generates static websites using a Markdown and any #lang")
 (define scribblings '(("scribblings/polyglot.scrbl" ())))
