f5dfb7b883d7c1df59be8fa984469bdf620239e8
Sage Gerard <sage@sagegerard.com>
2019-09-19 17:36:28 -0400

    Reload changed modules

--- private/racket-as-asset.rkt
+++ private/racket-as-asset.rkt
@@ -2,9 +2,11 @@
 
 (provide (all-defined-out))
 (require racket/contract
+         racket/rerequire
          unlike-assets
          unlike-assets/logging)
 
 (define/contract (delegate-to-asset-module clear compiler) advance/c
   (<info "Delegating to ~a's write-dist-file" clear)
+  (dynamic-rerequire clear)
   (dynamic-require clear 'write-dist-file))
