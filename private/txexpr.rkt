7631741cb18dfa8990e6a05628cae726fbeeb8dd
Sage Gerard <sage@sagegerard.com>
2019-09-30 19:51:52 -0400

    Refactor txexpr.rkt

--- private/txexpr.rkt
+++ private/txexpr.rkt
@@ -3,145 +3,83 @@
 ;;; Provides operations used in this project for tagged X-expressions
 
 (require
-  racket/function
-  racket/list
   (submod txexpr safe)
   xml)
 
 (provide
-  expand-forest
   (all-from-out
     (submod txexpr safe)
     xml)
-  splitf-txexpr/root
-  has-target-children?
-  expand-children)
+  (all-defined-out))
 
-(define (has-target-children? target? x)
-  (and (list? x)
-       (findf target? (get-elements x))
-       #t))
+(define (tag-equal? t tx)
+  (and (txexpr? tx)
+       (equal? t (get-tag tx))))
 
-(define (expand-children target? expand x)
-  (txexpr 
-    (get-tag x)
-    (get-attrs x)
-    (foldl
+;; Like splitf-txexpr, except:
+;;   - Each matching element can be replaced by
+;;     more than one element.
+;;   - Control will not leave the procedure
+;;     until all possible replacements are made.
+(define (refoster tx replace? replace)
+  (define (has-target-children? x)
+    (and (list? x)
+         (findf replace? (get-elements x))
+         #t))
+
+  (define (replace-children x)
+    (txexpr
+     (get-tag x)
+     (get-attrs x)
+     (foldl
       (λ (kid res)
-         (append res
-                 (if (target? kid)
-                     (expand kid)
-                     (list kid))))
+        (append res
+                (if (replace? kid)
+                    (replace kid)
+                    (list kid))))
       null
       (get-elements x))))
 
-;; splitf-txexpr breaks when replacing the root, so handle that case here.
-(define (splitf-txexpr/root tx match? [replace-proc (λ _ #f)])
-  (if (match? tx)
-      (values (replace-proc tx) (cons tx null))
-      (splitf-txexpr tx match? replace-proc)))
+  (let-values ([(new-content replaced)
+                (splitf-txexpr
+                 tx
+                 has-target-children?
+                 replace-children)])
+    (if (> (length replaced) 0)
+        (refoster new-content replace? replace)
+        new-content)))
 
+;; *: A mock root element lets us treat top-level
+;; elements like subtrees, simplifying the problem.
+(define (interlace-txexpr elements replace?/list replace/list)
+  (define elements/after
+    (get-elements
+     (foldl
+      (λ (replace? replace tx)
+        (refoster tx
+                  replace?
+                  replace))
+      (cons 'root elements) ; *
+      replace?/list
+      replace/list)))
 
-#|
-Replace one node with several within a containing node.
+  ; Optimize by tracking if a replacement
+  ; occurred if performance becomes an issue.
+  (if (not (equal? elements elements/after))
+      (interlace-txexpr elements/after replace?/list replace/list)
+      elements/after))
 
-(expand-nested-target '((p (i) (strong)) (p (i)))
-                      (λ (x) (equal? (get-tag x) 'i))
-                      (λ (x) '((b) (b))))
-
-yields: '((p (b) (b) (strong)) (p (b) (b)))
-|#
-(define (expand-nested-target-iter initial-xexpr process? process)
-  (let loop ([prev initial-xexpr])
-    (if (txexpr? prev)
-      (let-values ([(next replaced)
-                    (splitf-txexpr/root prev process? process)])
-        (if (> (length replaced) 0) (loop next) next))
-      #f)))
-
-(define (expand-nested-target elements target? expand)
-  (define process? (curry has-target-children? target?))
-  (define (process node)
-    (define element (expand-children target? expand node))
-    (if (empty? (get-elements element))
-        #f
-        element))
-  (foldl
-    (λ (initial-xexpr res)
-      (define expanded (expand-nested-target-iter initial-xexpr
-                                                  process?
-                                                  process))
-      (if expanded
-          (append res (list expanded))
-          res))
-    null
-    elements))
-
-
-#|
-Replace one node with several at the top level.
-
-(expand-nested-target '((i) (b) (i))
-                      (λ (x) (equal? (get-tag x) 'i))
-                      (λ (x) '((b) (b))))
-
-yields: '((b) (b) (b) (b) (b))
-|#
-(define (expand-top-level-iter elements target? expand)
-  (define replaced? #f)
-  (define expanded
-    (foldl
-      (λ (tx res)
-         (append res
-           (if (target? tx)
-               (begin (set! replaced? #t)
-                      (expand tx))
-               (list tx))))
-      null
-      elements))
-
-  (values expanded replaced?))
-
-(define (expand-top-level elements target? expand)
-  (let loop ([prev elements])
-    (define-values (expanded replaced?) (expand-top-level-iter prev target? expand))
-    (if replaced? (loop expanded) expanded)))
-
-;; Combines above cases into one call.
-(define (expand-forest elements target? expand)
-  (expand-nested-target
-    (expand-top-level elements target? expand)
-    target?
-    expand))
 
 (module+ test
-  (require rackunit)
-  (define (i-element? x)
-    (and (txexpr? x) (equal? (get-tag x) 'i)))
+  (require rackunit racket/function)
+  (define i-element? (curry tag-equal? 'i))
+  (define s-element? (curry tag-equal? 's))
 
-  (test-case "Existential check of target child elements"
-    (check-true  (has-target-children? i-element? '(span "a" (i) "b")))
-    (check-false (has-target-children? i-element? '(span "a" "b"))))
-
-  (test-equal? "Replace target with nothing"
-    (expand-children
-      i-element?
-      (λ _ null)
-      '(span "a" (i) "b" (i) (i) "c" (i)))
-    '(span "a" "b" "c"))
-
-  (test-equal? "Replace target with fragments"
-    (expand-children
-      i-element?
-      (λ _ '((b) (b)))
-      '(span "a" (i) "b" (i) "c" (i)))
-    '(span "a" (b) (b) "b" (b) (b) "c" (b) (b)))
-
-
-  (test-equal? "Replace target with fragments in element forest"
-    (expand-forest
-      '((i) (p (i) (i)) (b) (i) (b (i)) (i))
-      i-element?
-      (λ _ '((b) (b))))
-    '((b) (b) (p (b) (b) (b) (b)) (b) (b) (b) (b (b) (b)) (b) (b))))
-
+  (test-equal? "interlace-txexpr: one pass"
+    (interlace-txexpr
+      '((i) (p (i) (i)) (b) (s) (b (i)) (i))
+      (list i-element?
+            s-element?)
+      (list (λ _ '((b) (b)))
+            (λ _ '((x) (x) (x)))))
+    '((b) (b) (p (b) (b) (b) (b)) (b) (x) (x) (x) (b (b) (b)) (b) (b))))
