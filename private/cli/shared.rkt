59daf3db76588604daac846071d0e471bae540ec
Sage Gerard <sage@sagegerard.com>
2019-10-07 10:13:33 -0400

    Extract E2E test

--- private/cli/shared.rkt
+++ private/cli/shared.rkt
@@ -5,12 +5,12 @@
 (require racket/class
          racket/dict
          racket/file
+         racket/rerequire
          unlike-assets/logging
          "../../main.rkt"
          "../fs.rkt")
 
 (define polyglot-class/cli-asserted (make-parameter #f))
-(define test-project-path (system-temp-rel "polyglot-test"))
 
 (define (log-exn e)
   (define string-port (open-output-string))
@@ -36,32 +36,10 @@
   (make-directory* (dist-rel))
   (empty-directory (dist-rel)))
 
-
-(define (fresh-test-project skel)
-  (when (directory-exists? test-project-path)
-    (delete-directory/files test-project-path))
-  (copy-skeleton skel test-project-path))
-
 (define (make-compiler)
   (new (or (polyglot-class/cli-asserted)
-           (dynamic-require (project-rel ".polyglotrc.rkt")
-                            'polyglot+%
-                            (λ _ polyglot/imperative%)))))
-
-(module+ test
-  (require racket/class
-           rackunit)
-
-  (for ([workflow '("functional" "imperative")]
-        [cls (list polyglot/functional% polyglot/imperative%)])
-    (test-case
-      (format "Can build test project (~a)" workflow)
-      (with-handlers ([exn? (λ (e) ((error-display-handler) (exn-message e) e))])
-        (define test-project-path (fresh-test-project workflow))
-        (parameterize ([polyglot-project-directory test-project-path])
-          (define compiler (make-compiler))
-          (clear-distribution!)
-          (send compiler add! (assets-rel "index.md"))
-          (send compiler compile!)
-          (check-equal? (length (directory-list (assets-rel)))
-                        (length (directory-list (dist-rel)))))))))
+           (let ([rcfile (project-rel ".polyglotrc.rkt")])
+             (dynamic-rerequire rcfile)
+             (dynamic-require rcfile
+                              'polyglot+%
+                              (λ _ polyglot/imperative%))))))
