041500c93b5d37a3ef49cefd65b0d2ff2f9d7870
Sage Gerard <sage@sagegerard.com>
2020-02-13 12:19:06 -0500

    Add aws dependency for tests

--- polyglot-test/info.rkt
+++ polyglot-test/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "polyglot-lib"))
+(define deps '("base" "polyglot-lib" "aws"))
 (define build-deps '("rackunit-lib"))
 (define pkg-desc "Tests for polyglot")
 (define pkg-authors '(sage))
