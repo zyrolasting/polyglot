2b015559370dff22003a023ed75a8cf1d6df3a67
Sage Gerard <sage@sagegerard.com>
2020-02-13 12:31:13 -0500

    Adapt to directory-exists? contract in paths test

--- polyglot-test/tests/polyglot/paths.rkt
+++ polyglot-test/tests/polyglot/paths.rkt
@@ -2,7 +2,8 @@
 
 (require racket/file
          rackunit
-         polyglot/paths)
+         polyglot/paths
+         (submod polyglot/paths test))
 
 (test-case "make-dist-path-string"
   (test-equal? "Produce relative paths as strings given two complete paths"
@@ -15,15 +16,17 @@
                "/"))
 
 (test-case "Users may override derived paths"
-  (define (check-fallbacks)
-    (check-equal? (polyglot-assets-directory) (build-path (polyglot-project-directory) "assets"))
-    (check-equal? (polyglot-dist-directory) (build-path (polyglot-project-directory) "dist")))
-  (dynamic-wind
-    check-fallbacks
-    (λ _
-      (parameterize ([polyglot-project-directory (current-directory)]
-                     [polyglot-assets-directory (current-directory)]
-                     [polyglot-dist-directory (current-directory)])
-        (check-equal? (polyglot-assets-directory) (polyglot-dist-directory))
-        (check-equal? (polyglot-assets-directory) (polyglot-project-directory))))
-    check-fallbacks))
+  (define skel-path (car skel-paths))
+  (parameterize ([polyglot-project-directory skel-path])
+    (define (check-fallbacks)
+      (check-equal? (polyglot-assets-directory) (build-path (polyglot-project-directory) "assets"))
+      (check-equal? (polyglot-dist-directory) (build-path (polyglot-project-directory) "dist")))
+    (dynamic-wind
+      check-fallbacks
+      (λ _
+        (parameterize ([polyglot-project-directory skel-path]
+                       [polyglot-assets-directory skel-path]
+                       [polyglot-dist-directory skel-path])
+          (check-equal? (polyglot-assets-directory) (polyglot-dist-directory))
+          (check-equal? (polyglot-assets-directory) (polyglot-project-directory))))
+      check-fallbacks)))
