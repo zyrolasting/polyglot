bef3ed142409ec65da38df00ce86f2444dafd196
Sage Gerard <sage@sagegerard.com>
2020-02-13 18:09:44 -0500

    Fix info.rkt for docs

---
+++ polyglot-doc/polyglot/info.rkt
@@ -0,0 +1,5 @@
+#lang info
+(define scribblings '(("scribblings/guide/polyglot-guide.scrbl" (multi-page) (tools))
+                      ("scribblings/reference/polyglot-reference.scrbl" (multi-page) (library))
+                      ("scribblings/how-tos/polyglot-how-tos.scrbl" (multi-page) (omit-start))
+                      ("scribblings/tutorials/polyglot-tutorials.scrbl" (multi-page) (omit-start))))
