5c95a96500fe69cbd2704f1735f2a5d9dc5f1545
Sage Gerard <sage@sagegerard.com>
2020-02-16 11:30:11 -0500

    Remove polyglot-test from metapkg deps

--- polyglot/info.rkt
+++ polyglot/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "polyglot-lib" "polyglot-doc" "polyglot-test"))
+(define deps '("base" "polyglot-lib" "polyglot-doc"))
 (define implies '("polyglot-lib" "polyglot-doc"))
 (define pkg-desc "A meta-package for polyglot-*")
 (define pkg-authors '(sage))
