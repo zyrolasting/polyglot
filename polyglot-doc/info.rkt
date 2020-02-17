9f90eb5ad2b796b53d6fd92a2212e4acc745d18b
Sage Gerard <sage@sagegerard.com>
2020-02-17 16:25:26 -0500

    Add missing doc deps

--- polyglot-doc/info.rkt
+++ polyglot-doc/info.rkt
@@ -1,6 +1,14 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "polyglot-lib" "unlike-assets-doc" "css-expr"))
+(define deps '("aws"
+               "base"
+               "css-expr"
+               "file-watchers"
+               "markdown"
+               "polyglot-lib"
+               "txexpr"
+               "unlike-assets-doc"
+               "unlike-assets-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define pkg-desc "Documentation for Polyglot")
 (define update-implies '("polyglot-lib"))
