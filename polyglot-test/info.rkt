a1fda5d768b38c96252757767f42711fff2df35f
Sage Gerard <sage@sagegerard.com>
2020-02-18 12:51:55 -0500

    Add missing deps

--- polyglot-test/info.rkt
+++ polyglot-test/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "polyglot-lib" "aws"))
+(define deps '("base" "polyglot-lib" "aws" "unlike-assets-lib" "markdown"))
 (define build-deps '("rackunit-lib"))
 (define pkg-desc "Tests for polyglot")
 (define pkg-authors '(sage))
