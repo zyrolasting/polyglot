921eb580fc31b9f91f64108461e88873394543e0
Sage Gerard <sage@sagegerard.com>
2020-04-28 23:45:39 -0400

    Add missing version declaration

--- polyglot-lib/polyglot/info.rkt
+++ polyglot-lib/polyglot/info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define raco-commands
   '(("polyglot" (submod polyglot/private/cli/entry main) "polyglot CLI" #f)))
+(define version "2.8")
 (define racket-launcher-libraries '("private/cli/entry.rkt"))
 (define racket-launcher-names '("polyglot"))
 (define compile-omit-paths '("private/skel"))
